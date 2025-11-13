ddd608ac2e3d0efabb66aa038c7ee1cadec46b0c
Wei-Chen Lai <douhualu6@gmail.com>
2025-11-13 11:29:33 +0800

    init lab4

---
+++ lab4/file/FIFO_sync.v
@@ -0,0 +1,17 @@
+module FIFO_sync(
+    input             clk     ,
+    input             rst     ,
+    input             wr_en   ,
+    input             rd_en   ,
+    input       [7:0] data_in ,
+    output            full    ,
+    output            empty   ,
+    output      [7:0] data_out
+);
+
+/*
+	Write Your Design Here ~
+*/
+
+
+endmodule
